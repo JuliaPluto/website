f08c1a77db146e59e9b0edbea45003a0a5c702d2
Fons van der Plas <fonsvdplas@gmail.com>
2022-03-14 22:51:32 +0100

    improve error logs

--- generate.jl
+++ generate.jl
@@ -6,4 +6,21 @@ s = Pluto.ServerSession()
 s.options.server.launch_browser = false
 
 nb = Pluto.SessionActions.open(s, joinpath(@__DIR__, "PlutoPages.jl"); run_async=false)
+
+write("generation_report.html", Pluto.generate_html(generate_html))
+
+for c in nb.cells
+    if c.errored == "code"
+        println("Cell errored: ", c.cell_id)
+        println()
+        show(io, MIME"text/plain"(), c.output.body)
+        println()
+        println()
+    end
+end
+
 Pluto.SessionActions.shutdown(s, nb)
+
+if any(c -> c.errored, nb.cells)
+    exit(1)
+end

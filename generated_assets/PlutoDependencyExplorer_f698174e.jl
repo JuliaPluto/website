4f95637b3d5ab3118850830d09a8989e14d5ebfd
fonsp <fonsvdplas@gmail.com>
2024-01-21 10:51:07 +0000

    Deploying to dist from  @ 58123d99d77f5e277f505c9a1d92dcbad4e96e12 🚀

--- generated_assets/PlutoDependencyExplorer_f698174e.jl
+++ generated_assets/PlutoDependencyExplorer_f698174e.jl
@@ -28,6 +28,42 @@ md"""
 # ╔═╡ f10e5aee-a5da-45aa-89d6-8e47835ceffd
 Docs.Binding(PlutoDependencyExplorer, :PlutoDependencyExplorer)
 
+# ╔═╡ d917d940-3de5-4f0a-a83e-33c0522b2c83
+html"""
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Jaldi:wght@400;700&display=swap');
+
+pluto-output {
+	font-family: 'Jaldi', sans-serif;
+	overflow-y: hidden;
+}
+
+pluto-output h1, pluto-output h2, pluto-output h3, pluto-output h4, pluto-output h5, pluto-output h6 {
+font-family: Jaldi, sans-serif;
+	line-height: 1;
+}
+
+pluto-output .pluto-docs-binding h1 {
+	font-size: 1.4em;
+}
+
+pluto-output h1 {
+	margin-block-start: 2.5em !important;
+}
+
+pluto-output .pluto-docs-binding h2 {
+    font-size: 1.3em;
+}
+pluto-output .pluto-docs-binding h3,
+pluto-output .pluto-docs-binding h4,
+pluto-output .pluto-docs-binding h5,
+pluto-output .pluto-docs-binding h6
+{
+    font-size: 1.1em;
+}
+</style>
+"""
+
 # ╔═╡ cf44eaae-7868-464a-8d17-5a5335977a3c
 md"""
 # Basic example
@@ -280,46 +316,6 @@ Docs.Binding(EEE, :pretransform_pluto)
 
 # ╔═╡ effa9b86-f78f-47a7-898b-308a12c6fee7
 import PlutoUI
-
-# ╔═╡ d917d940-3de5-4f0a-a83e-33c0522b2c83
-html"""
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Jaldi:wght@400;700&display=swap');
-
-pluto-output {
-	font-family: 'Jaldi', sans-serif;
-	overflow-y: hidden;
-}
-
-html {
-	font-size: 20px;
-}
-
-pluto-output h1, pluto-output h2, pluto-output h3, pluto-output h4, pluto-output h5, pluto-output h6 {
-font-family: Jaldi, sans-serif;
-	line-height: 1;
-}
-
-pluto-output .pluto-docs-binding h1 {
-	font-size: 1.4em;
-}
-
-pluto-output h1 {
-	margin-block-start: 2.5em !important;
-}
-
-pluto-output .pluto-docs-binding h2 {
-    font-size: 1.3em;
-}
-pluto-output .pluto-docs-binding h3,
-pluto-output .pluto-docs-binding h4,
-pluto-output .pluto-docs-binding h5,
-pluto-output .pluto-docs-binding h6
-{
-    font-size: 1.1em;
-}
-</style>
-"""
 
 # ╔═╡ 8dac5863-86e0-483c-bce2-f396c698ddb3
 pkgversion(PlutoDependencyExplorer)
@@ -618,6 +614,7 @@ version = "17.4.0+2"
 # ╟─488a0b76-0d9e-46cc-8db0-f7e072e846f5
 # ╠═8b669928-eb0d-47b6-bea3-da46e98291d3
 # ╟─f10e5aee-a5da-45aa-89d6-8e47835ceffd
+# ╟─d917d940-3de5-4f0a-a83e-33c0522b2c83
 # ╟─cf44eaae-7868-464a-8d17-5a5335977a3c
 # ╠═deff4ae6-85a7-4003-bef0-6c895eb5ddad
 # ╠═43f09b34-6851-46bc-ba47-8616183ef054
@@ -682,7 +679,6 @@ version = "17.4.0+2"
 # ╟─1c53a35f-45ff-46a7-90f5-8dab7bf6cc0b
 # ╟─c96381eb-ca8d-498e-bfa4-06a107b1a24a
 # ╟─effa9b86-f78f-47a7-898b-308a12c6fee7
-# ╟─d917d940-3de5-4f0a-a83e-33c0522b2c83
 # ╠═8dac5863-86e0-483c-bce2-f396c698ddb3
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002

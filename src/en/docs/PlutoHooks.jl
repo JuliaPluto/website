73e6f79d5b86f303e558e81d910b12a6100236b5
Boshra Ariguib <boshra.ariguib@posteo.net>
2026-03-16 16:23:11 +0100

    Added documentation. Related to #37

--- src/en/docs/PlutoHooks.jl
+++ src/en/docs/PlutoHooks.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.20.21
+# v0.20.23
 
 using Markdown
 using InteractiveUtils
@@ -7,11 +7,11 @@ using InteractiveUtils
 # ╔═╡ 9c8b8c7c-05bd-11f1-bde1-5302de07152b
 using PlutoHooks
 
-# ╔═╡ df12ed30-1e5c-4b32-8bf0-08ab90fc3256
-TODO: intro
+# ╔═╡ bf5f78c3-d29f-4218-a963-6d94f55fa9e2
+Markdown.parse(read(joinpath(pkgdir(PlutoHooks), "README.md"), String))
 
-# ╔═╡ d493f0db-e3ff-4fe1-9ce9-621ebf2e415a
-
+# ╔═╡ 9bd0c8eb-ecf4-424f-a507-5face6e192b0
+md"A fuller overview can be found under: [https://juliapluto.github.io/PlutoHooks.jl/src/notebook.html](A fuller overview can be found under: `https://juliapluto.github.io/PlutoHooks.jl/src/notebook.html`)"
 
 # ╔═╡ 9e93194f-4e08-44f3-9aff-6a18f7ef48ca
 md"""
@@ -23,6 +23,24 @@ Docs.Binding(PlutoHooks, Symbol("@use_deps"))
 
 # ╔═╡ aa3f1c57-8a50-44f4-be93-665c5c2d0c3d
 Docs.Binding(PlutoHooks, Symbol("@use_effect"))
+
+# ╔═╡ 82c06fd7-f531-4130-89fd-f141d6f2492b
+Docs.Binding(PlutoHooks, Symbol("@use_memo"))
+
+# ╔═╡ af07648e-6d5d-4833-958b-fba2479fc7b2
+Docs.Binding(PlutoHooks, Symbol("@use_state"))
+
+# ╔═╡ e7739a9d-988d-4fdf-a693-f52de3b8b4bd
+Docs.Binding(PlutoHooks, Symbol("@use_ref"))
+
+# ╔═╡ 6219f6af-cb4d-4476-bbd7-8a6f1b46414d
+Docs.Binding(PlutoHooks, Symbol("@use_is_pluto_cell"))
+
+# ╔═╡ 9b54cd0d-ef35-4c82-b460-27fb3772e888
+Docs.Binding(PlutoHooks, Symbol("@skip_as_script"))
+
+# ╔═╡ a5c61ba5-0401-4427-9086-53af9ef5df94
+Docs.Binding(PlutoHooks, Symbol("@only_as_script"))
 
 # ╔═╡ 5449d62d-54e4-459a-ad4b-f00388aa923f
 pkgversion(PlutoHooks)
@@ -40,7 +58,7 @@ PlutoHooks = "~0.1.0"
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.12.2"
+julia_version = "1.12.5"
 manifest_format = "2.0"
 project_hash = "3264ce90cd39281c6868327ed317b09f52d0dc0e"
 
@@ -90,11 +108,17 @@ version = "1.11.0"
 
 # ╔═╡ Cell order:
 # ╠═9c8b8c7c-05bd-11f1-bde1-5302de07152b
-# ╠═df12ed30-1e5c-4b32-8bf0-08ab90fc3256
-# ╠═d493f0db-e3ff-4fe1-9ce9-621ebf2e415a
+# ╟─bf5f78c3-d29f-4218-a963-6d94f55fa9e2
+# ╟─9bd0c8eb-ecf4-424f-a507-5face6e192b0
 # ╟─9e93194f-4e08-44f3-9aff-6a18f7ef48ca
 # ╟─5e415434-71a5-4642-a0e3-d2299b263ba2
 # ╟─aa3f1c57-8a50-44f4-be93-665c5c2d0c3d
+# ╟─82c06fd7-f531-4130-89fd-f141d6f2492b
+# ╟─af07648e-6d5d-4833-958b-fba2479fc7b2
+# ╟─e7739a9d-988d-4fdf-a693-f52de3b8b4bd
+# ╟─6219f6af-cb4d-4476-bbd7-8a6f1b46414d
+# ╟─9b54cd0d-ef35-4c82-b460-27fb3772e888
+# ╟─a5c61ba5-0401-4427-9086-53af9ef5df94
 # ╠═5449d62d-54e4-459a-ad4b-f00388aa923f
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002

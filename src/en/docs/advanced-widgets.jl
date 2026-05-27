79936f8d466ca14436ceb71f2e0258e3360e6034
Fons van der Plas <fonsvdplas@gmail.com>
2026-05-27 15:37:05 +0200

    Update advanced-widgets.jl

--- src/en/docs/advanced-widgets.jl
+++ src/en/docs/advanced-widgets.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.20.14
+# v1.0.0
 
 #> [frontmatter]
 #> image = "https://github.com/fonsp/Pluto.jl/assets/6933510/0a27e62f-6ef1-4f13-b47c-2a2cefa8850a"
@@ -30,11 +30,23 @@ macro bind(def, element)
     #! format: on
 end
 
-# ╔═╡ a17a8282-05f6-469d-9579-63029db89f79
-using HypertextLiteral
+# ╔═╡ d2e3a7f6-52a8-43bb-9edf-be13aecaff21
+begin
+	import Pkg
+	pde = joinpath(pwd(), "..", "..", "..", "pluto-deployment-environment")
+	if isdir(pde)
+		Pkg.activate(pde, io=devnull)
+		Pkg.instantiate(io=devnull)
+	else
+		@warn "Notebook launched outside of plutojl.org website repository... Installing packages from registry."
+		Pkg.activate(temp=true)
+		Pkg.add(["HypertextLiteral", "AbstractPlutoDingetjes", "PlutoUI"])
+	end
+	import AbstractPlutoDingetjes
+	using HypertextLiteral, PlutoUI
 
-# ╔═╡ 810b3d50-00e0-11ef-034d-73dc15b2f8bd
-using PlutoUI
+	Text("Packages loaded")
+end
 
 # ╔═╡ 1913bd73-41fb-437a-9505-4e0f24d62ac6
 md"""
@@ -61,13 +73,21 @@ TableOfContents(include_definitions=true)
 md"""
 
 # Custom input
-PlutoUI.jl provides some basic inputs, like sliders, textfields and more. But you can also make your own specialised widget! For example, you could make a
-- [map location picker](https://github.com/lukavdplas/PlutoMapPicker.jl), showing a map, and giving back the clicked coordinate with `@bind`.
-  ![screenshot](https://github.com/lukavdplas/PlutoMapPicker.jl/raw/main/screenshot.png)
-- [chemical equation builder](https://youtu.be/lNbU5jNp67s?t=1546), giving back a `Catalyst.ReactionSystem` with `@bind`.
-  ![screenshot](https://github.com/JuliaPluto/Pluto.jl/assets/6933510/0a27e62f-6ef1-4f13-b47c-2a2cefa8850a)
-- widget composed with Markdown and PlutoUI, giving back 4 values in a tuple with `@bind`.
-  ![screenshot](https://user-images.githubusercontent.com/6933510/145588612-14824654-5c73-45f8-983c-8913c7101a78.png)
+PlutoUI.jl provides some basic inputs, like sliders, textfields and more. But you can also make your own specialised widget! For example, you could make:
+
+
+![screenshot of a map location picker](https://github.com/lukavdplas/PlutoMapPicker.jl/raw/main/screenshot.png)
+
+A [map location picker](https://github.com/lukavdplas/PlutoMapPicker.jl), showing a map, and giving back the clicked coordinate with `@bind`.
+
+![screenshot of a chemical equation builder](https://github.com/JuliaPluto/Pluto.jl/assets/6933510/0a27e62f-6ef1-4f13-b47c-2a2cefa8850a)
+
+A [chemical equation builder](https://youtu.be/lNbU5jNp67s?t=1546), giving back a `Catalyst.ReactionSystem` with `@bind`.
+
+
+![screenshot of a composed widget](https://user-images.githubusercontent.com/6933510/145588612-14824654-5c73-45f8-983c-8913c7101a78.png)
+
+A widget composed with Markdown and PlutoUI, giving back 4 values in a tuple with `@bind`.
  
 """
 
@@ -84,15 +104,6 @@ In some cases, you might just want to "wrap" and existing widget in static conte
 
 Widgets created with these methods can be published in a package!
 """
-
-# ╔═╡ b9926b7f-989f-47ed-8ab6-703d6ced1f21
-Docs.Binding(PlutoUI, :combine)
-
-# ╔═╡ 31cd3e04-48dd-40c2-8a29-78e7a0e8652b
-Docs.Binding(PlutoUI.Experimental, :transformed_value)
-
-# ╔═╡ 43eb1935-7eb7-40bc-9d8f-4c7e7e6d8f49
-Docs.Binding(PlutoUI.Experimental, :wrapped)
 
 # ╔═╡ eaa26f32-00c8-4c36-a8ee-3ff11e184e35
 md"""
@@ -274,8 +285,8 @@ You can use `PlutoUI.ExperimentalLayout` to display objects in columns, grids, a
 
 # ╔═╡ 0359ccba-8b57-47a7-a9a3-5d61419479f1
 PlutoUI.ExperimentalLayout.grid([
-	md"# Layout demo!"      Text("")
-	Text("I'm on the left") Dict(:a => 1, :b => [2,3])
+	md"#### Layout demo!"      Text("")
+	Text("I'm on the left")    Dict(:a => 1, :b => [2,3])
 ])
 
 # ╔═╡ 119e9c4e-c695-4268-8ec6-b5a6f8d6f877
@@ -384,6 +395,40 @@ html"""
 <a href="https://plutojl.org/en/docs/abstractplutodingetjes/" class="arrow">Find more in the AbstractPlutoDingetjes docs</a>
 """
 
+# ╔═╡ 6ea4e81c-4afc-4414-8d03-daccdbe054c4
+HiddenDocs(mod, name) = details(
+	@htl("Show docstring for <code>$name</code>"), 
+	@htl """
+	<div class="pluto-docs-binding">
+	<span id="$(name)">$(name)</span>
+	$(Base.Docs.doc(Base.Docs.Binding(mod, name)))
+	</div>
+	""")
+
+# ╔═╡ b9926b7f-989f-47ed-8ab6-703d6ced1f21
+HiddenDocs(PlutoUI, :combine)
+
+# ╔═╡ 31cd3e04-48dd-40c2-8a29-78e7a0e8652b
+HiddenDocs(PlutoUI.Experimental, :transformed_value)
+
+# ╔═╡ 43eb1935-7eb7-40bc-9d8f-4c7e7e6d8f49
+HiddenDocs(PlutoUI.Experimental, :wrapped)
+
+# ╔═╡ 0fff3ea7-74a5-4bd5-a205-db21b23a2601
+HiddenDocs(AbstractPlutoDingetjes.Display, :published_to_js)
+
+# ╔═╡ 2320e06c-56f8-47ac-b532-a85ae2cbe3f5
+HiddenDocs(AbstractPlutoDingetjes.Display, :with_js_link)
+
+# ╔═╡ 08293d88-66ff-433b-bd74-d53279235c88
+HiddenDocs(AbstractPlutoDingetjes, :is_inside_pluto)
+
+# ╔═╡ 3775fb8c-ec06-4482-9b0e-a4c1aa32afb7
+HiddenDocs(AbstractPlutoDingetjes, :is_supported_by_display)
+
+# ╔═╡ 404872d6-675e-4ac7-be0f-5e0dce836bfa
+HiddenDocs(AbstractPlutoDingetjes.Display, Symbol("@embed"))
+
 # ╔═╡ 965e17ea-cc2c-4072-82c4-94f259ce9224
 md"""
 # Distributing widgets
@@ -425,337 +470,6 @@ end
 # ╔═╡ 7cea0a79-d139-4ed4-a0e5-ea1aae4c219e
 demo_html = @htl("<b style='font-family: cursive;'>Hello!</b>")
 
-# ╔═╡ 86c77330-d553-4e04-8e7e-38de72626bba
-md"""
-### Embeddable output
-
-Pluto has a multimedia object viewer, which is used to display the result of a cell's output. Depending on the _type_ of the resulting object, the richest possible viewer is used. This includes:
-- an interactive structure viewer for arrays, tables, dictionaries and more: 
-  $(embed_display([1,2,(a=3, b=4)]))
-- an `<img>` tag with optimized data transfer for images: $(embed_display(demo_img))
-- raw HTML for HTML-showable objects: $(embed_display(demo_html))
-
-Normally, you get this object viewer for the _output_ of a cell. However, as demonstrated in the list above, you can also **embed Pluto's object viewer in your own HTML**. To do so, Pluto provides a function:
-```julia
-embed_display(x)
-```
-
-#### Example
-
-As an example, here is how you display two arrays side-by-side:
-
-```julia
-@htl("\""
-
-<div style="display: flex;">
-$(embed_display(rand(4)))
-$(embed_display(rand(4)))
-</div>
-
-"\"")
-```
-
-Currently, you use this function with `PlutoRunner.embed_display` or just `embed_display`. In the future, this will be `AbstractPlutoDingetjes.Display.embed_display`, please comment [this PR](https://github.com/JuliaPluto/AbstractPlutoDingetjes.jl/pull/9) if you want it!
-
-You can [learn more](https://github.com/fonsp/Pluto.jl/pull/1126) about how this feature works, or how to use it inside packages.
-"""
-
-# ╔═╡ e5a05101-a023-40ad-9bef-c6c8d18eb719
-import AbstractPlutoDingetjes
-
-# ╔═╡ 0fff3ea7-74a5-4bd5-a205-db21b23a2601
-Docs.Binding(AbstractPlutoDingetjes.Display, :published_to_js)
-
-# ╔═╡ 2320e06c-56f8-47ac-b532-a85ae2cbe3f5
-Docs.Binding(AbstractPlutoDingetjes.Display, :with_js_link)
-
-# ╔═╡ 08293d88-66ff-433b-bd74-d53279235c88
-Docs.Binding(AbstractPlutoDingetjes, :is_inside_pluto)
-
-# ╔═╡ 3775fb8c-ec06-4482-9b0e-a4c1aa32afb7
-Docs.Binding(AbstractPlutoDingetjes, :is_supported_by_display)
-
-# ╔═╡ 00000000-0000-0000-0000-000000000001
-PLUTO_PROJECT_TOML_CONTENTS = """
-[deps]
-AbstractPlutoDingetjes = "6e696c72-6542-2067-7265-42206c756150"
-HypertextLiteral = "ac1192a8-f4b3-4bfe-ba22-af5b92cd3ab2"
-PlutoUI = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
-
-[compat]
-AbstractPlutoDingetjes = "~1.3.2"
-HypertextLiteral = "~0.9.5"
-PlutoUI = "~0.7.59"
-"""
-
-# ╔═╡ 00000000-0000-0000-0000-000000000002
-PLUTO_MANIFEST_TOML_CONTENTS = """
-# This file is machine-generated - editing it directly is not advised
-
-julia_version = "1.10.0"
-manifest_format = "2.0"
-project_hash = "7965676cd23ebed3ba799477cbe6198412888fe8"
-
-[[deps.AbstractPlutoDingetjes]]
-deps = ["Pkg"]
-git-tree-sha1 = "6e1d2a35f2f90a4bc7c2ed98079b2ba09c35b83a"
-uuid = "6e696c72-6542-2067-7265-42206c756150"
-version = "1.3.2"
-
-[[deps.ArgTools]]
-uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
-version = "1.1.1"
-
-[[deps.Artifacts]]
-uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
-
-[[deps.Base64]]
-uuid = "2a0f44e3-6c83-55bd-87e4-b1978d98bd5f"
-
-[[deps.ColorTypes]]
-deps = ["FixedPointNumbers", "Random"]
-git-tree-sha1 = "b10d0b65641d57b8b4d5e234446582de5047050d"
-uuid = "3da002f7-5984-5a60-b8a6-cbb66c0b333f"
-version = "0.11.5"
-
-[[deps.CompilerSupportLibraries_jll]]
-deps = ["Artifacts", "Libdl"]
-uuid = "e66e0078-7015-5450-92f7-15fbd957f2ae"
-version = "1.0.5+1"
-
-[[deps.Dates]]
-deps = ["Printf"]
-uuid = "ade2ca70-3891-5945-98fb-dc099432e06a"
-
-[[deps.Downloads]]
-deps = ["ArgTools", "FileWatching", "LibCURL", "NetworkOptions"]
-uuid = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
-version = "1.6.0"
-
-[[deps.FileWatching]]
-uuid = "7b1f6079-737a-58dc-b8bc-7a2ca5c1b5ee"
-
-[[deps.FixedPointNumbers]]
-deps = ["Statistics"]
-git-tree-sha1 = "335bfdceacc84c5cdf16aadc768aa5ddfc5383cc"
-uuid = "53c48c17-4a7d-5ca2-90c5-79b7896eea93"
-version = "0.8.4"
-
-[[deps.Hyperscript]]
-deps = ["Test"]
-git-tree-sha1 = "179267cfa5e712760cd43dcae385d7ea90cc25a4"
-uuid = "47d2ed2b-36de-50cf-bf87-49c2cf4b8b91"
-version = "0.0.5"
-
-[[deps.HypertextLiteral]]
-deps = ["Tricks"]
-git-tree-sha1 = "7134810b1afce04bbc1045ca1985fbe81ce17653"
-uuid = "ac1192a8-f4b3-4bfe-ba22-af5b92cd3ab2"
-version = "0.9.5"
-
-[[deps.IOCapture]]
-deps = ["Logging", "Random"]
-git-tree-sha1 = "8b72179abc660bfab5e28472e019392b97d0985c"
-uuid = "b5f81e59-6552-4d32-b1f0-c071b021bf89"
-version = "0.2.4"
-
-[[deps.InteractiveUtils]]
-deps = ["Markdown"]
-uuid = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
-
-[[deps.JSON]]
-deps = ["Dates", "Mmap", "Parsers", "Unicode"]
-git-tree-sha1 = "31e996f0a15c7b280ba9f76636b3ff9e2ae58c9a"
-uuid = "682c06a0-de6a-54ab-a142-c8b1cf79cde6"
-version = "0.21.4"
-
-[[deps.LibCURL]]
-deps = ["LibCURL_jll", "MozillaCACerts_jll"]
-uuid = "b27032c2-a3e7-50c8-80cd-2d36dbcbfd21"
-version = "0.6.4"
-
-[[deps.LibCURL_jll]]
-deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll", "Zlib_jll", "nghttp2_jll"]
-uuid = "deac9b47-8bc7-5906-a0fe-35ac56dc84c0"
-version = "8.4.0+0"
-
-[[deps.LibGit2]]
-deps = ["Base64", "LibGit2_jll", "NetworkOptions", "Printf", "SHA"]
-uuid = "76f85450-5226-5b5a-8eaa-529ad045b433"
-
-[[deps.LibGit2_jll]]
-deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll"]
-uuid = "e37daf67-58a4-590a-8e99-b0245dd2ffc5"
-version = "1.6.4+0"
-
-[[deps.LibSSH2_jll]]
-deps = ["Artifacts", "Libdl", "MbedTLS_jll"]
-uuid = "29816b5a-b9ab-546f-933c-edad1886dfa8"
-version = "1.11.0+1"
-
-[[deps.Libdl]]
-uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
-
-[[deps.LinearAlgebra]]
-deps = ["Libdl", "OpenBLAS_jll", "libblastrampoline_jll"]
-uuid = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
-
-[[deps.Logging]]
-uuid = "56ddb016-857b-54e1-b83d-db4d58db5568"
-
-[[deps.MIMEs]]
-git-tree-sha1 = "65f28ad4b594aebe22157d6fac869786a255b7eb"
-uuid = "6c6e2e6c-3030-632d-7369-2d6c69616d65"
-version = "0.1.4"
-
-[[deps.Markdown]]
-deps = ["Base64"]
-uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
-
-[[deps.MbedTLS_jll]]
-deps = ["Artifacts", "Libdl"]
-uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
-version = "2.28.2+1"
-
-[[deps.Mmap]]
-uuid = "a63ad114-7e13-5084-954f-fe012c677804"
-
-[[deps.MozillaCACerts_jll]]
-uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
-version = "2023.1.10"
-
-[[deps.NetworkOptions]]
-uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
-version = "1.2.0"
-
-[[deps.OpenBLAS_jll]]
-deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
-uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
-version = "0.3.23+2"
-
-[[deps.Parsers]]
-deps = ["Dates", "PrecompileTools", "UUIDs"]
-git-tree-sha1 = "8489905bcdbcfac64d1daa51ca07c0d8f0283821"
-uuid = "69de0a69-1ddd-5017-9359-2bf0b02dc9f0"
-version = "2.8.1"
-
-[[deps.Pkg]]
-deps = ["Artifacts", "Dates", "Downloads", "FileWatching", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
-uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
-version = "1.10.0"
-
-[[deps.PlutoUI]]
-deps = ["AbstractPlutoDingetjes", "Base64", "ColorTypes", "Dates", "FixedPointNumbers", "Hyperscript", "HypertextLiteral", "IOCapture", "InteractiveUtils", "JSON", "Logging", "MIMEs", "Markdown", "Random", "Reexport", "URIs", "UUIDs"]
-git-tree-sha1 = "ab55ee1510ad2af0ff674dbcced5e94921f867a9"
-uuid = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
-version = "0.7.59"
-
-[[deps.PrecompileTools]]
-deps = ["Preferences"]
-git-tree-sha1 = "5aa36f7049a63a1528fe8f7c3f2113413ffd4e1f"
-uuid = "aea7be01-6a6a-4083-8856-8a6e6704d82a"
-version = "1.2.1"
-
-[[deps.Preferences]]
-deps = ["TOML"]
-git-tree-sha1 = "9306f6085165d270f7e3db02af26a400d580f5c6"
-uuid = "21216c6a-2e73-6563-6e65-726566657250"
-version = "1.4.3"
-
-[[deps.Printf]]
-deps = ["Unicode"]
-uuid = "de0858da-6303-5e67-8744-51eddeeeb8d7"
-
-[[deps.REPL]]
-deps = ["InteractiveUtils", "Markdown", "Sockets", "Unicode"]
-uuid = "3fa0cd96-eef1-5676-8a61-b3b8758bbffb"
-
-[[deps.Random]]
-deps = ["SHA"]
-uuid = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
-
-[[deps.Reexport]]
-git-tree-sha1 = "45e428421666073eab6f2da5c9d310d99bb12f9b"
-uuid = "189a3867-3050-52da-a836-e630ba90ab69"
-version = "1.2.2"
-
-[[deps.SHA]]
-uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
-version = "0.7.0"
-
-[[deps.Serialization]]
-uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
-
-[[deps.Sockets]]
-uuid = "6462fe0b-24de-5631-8697-dd941f90decc"
-
-[[deps.SparseArrays]]
-deps = ["Libdl", "LinearAlgebra", "Random", "Serialization", "SuiteSparse_jll"]
-uuid = "2f01184e-e22b-5df5-ae63-d93ebab69eaf"
-version = "1.10.0"
-
-[[deps.Statistics]]
-deps = ["LinearAlgebra", "SparseArrays"]
-uuid = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
-version = "1.10.0"
-
-[[deps.SuiteSparse_jll]]
-deps = ["Artifacts", "Libdl", "libblastrampoline_jll"]
-uuid = "bea87d4a-7f5b-5778-9afe-8cc45184846c"
-version = "7.2.1+1"
-
-[[deps.TOML]]
-deps = ["Dates"]
-uuid = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
-version = "1.0.3"
-
-[[deps.Tar]]
-deps = ["ArgTools", "SHA"]
-uuid = "a4e569a6-e804-4fa4-b0f3-eef7a1d5b13e"
-version = "1.10.0"
-
-[[deps.Test]]
-deps = ["InteractiveUtils", "Logging", "Random", "Serialization"]
-uuid = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
-
-[[deps.Tricks]]
-git-tree-sha1 = "eae1bb484cd63b36999ee58be2de6c178105112f"
-uuid = "410a4b4d-49e4-4fbc-ab6d-cb71b17b3775"
-version = "0.1.8"
-
-[[deps.URIs]]
-git-tree-sha1 = "67db6cc7b3821e19ebe75791a9dd19c9b1188f2b"
-uuid = "5c2747f8-b7ea-4ff2-ba2e-563bfd36b1d4"
-version = "1.5.1"
-
-[[deps.UUIDs]]
-deps = ["Random", "SHA"]
-uuid = "cf7118a7-6976-5b1a-9a39-7adc72f591a4"
-
-[[deps.Unicode]]
-uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
-
-[[deps.Zlib_jll]]
-deps = ["Libdl"]
-uuid = "83775a58-1f1d-513f-b197-d71354ab007a"
-version = "1.2.13+1"
-
-[[deps.libblastrampoline_jll]]
-deps = ["Artifacts", "Libdl"]
-uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
-version = "5.8.0+1"
-
-[[deps.nghttp2_jll]]
-deps = ["Artifacts", "Libdl"]
-uuid = "8e850ede-7688-5339-a07c-302acd2aaf8d"
-version = "1.52.0+1"
-
-[[deps.p7zip_jll]]
-deps = ["Artifacts", "Libdl"]
-uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
-version = "17.4.0+2"
-"""
-
 # ╔═╡ Cell order:
 # ╟─1913bd73-41fb-437a-9505-4e0f24d62ac6
 # ╟─daddd959-7aa1-44b2-b40b-8e8407863b48
@@ -779,7 +493,7 @@ version = "17.4.0+2"
 # ╠═4da66c70-7d35-4a2f-a405-7b76555c0284
 # ╟─64b4a5a2-8708-40c7-8812-591c9e550d43
 # ╟─547f90c5-f8de-4a1c-9b9e-25bf8c78161e
-# ╠═e733647f-5af9-462c-9bb6-ef4282a04f6c
+# ╟─e733647f-5af9-462c-9bb6-ef4282a04f6c
 # ╟─b6b9a299-0d70-430c-8bf5-d10014261f27
 # ╠═0359ccba-8b57-47a7-a9a3-5d61419479f1
 # ╟─119e9c4e-c695-4268-8ec6-b5a6f8d6f877
@@ -799,13 +513,10 @@ version = "17.4.0+2"
 # ╟─e759af43-02c4-4408-8a48-bfc9adf4cfb7
 # ╟─08293d88-66ff-433b-bd74-d53279235c88
 # ╟─3775fb8c-ec06-4482-9b0e-a4c1aa32afb7
+# ╟─404872d6-675e-4ac7-be0f-5e0dce836bfa
 # ╟─ae310b31-92d4-49ce-bff8-5f51fea6afc8
-# ╟─86c77330-d553-4e04-8e7e-38de72626bba
+# ╟─6ea4e81c-4afc-4414-8d03-daccdbe054c4
 # ╟─965e17ea-cc2c-4072-82c4-94f259ce9224
 # ╟─35085d64-c324-43d6-a316-0edbf4a97bf1
 # ╟─7cea0a79-d139-4ed4-a0e5-ea1aae4c219e
-# ╠═a17a8282-05f6-469d-9579-63029db89f79
-# ╠═e5a05101-a023-40ad-9bef-c6c8d18eb719
-# ╠═810b3d50-00e0-11ef-034d-73dc15b2f8bd
-# ╟─00000000-0000-0000-0000-000000000001
-# ╟─00000000-0000-0000-0000-000000000002
+# ╟─d2e3a7f6-52a8-43bb-9edf-be13aecaff21
